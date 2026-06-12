3ad96dc2b89db9c73df875a6c24df39c9b5e7d3b
Paul FIEVET <pfievet@yahoo.fr>
2026-06-12 09:26:47 +0000

    Ajout Checkstyle, PMD, SpotBugs et JaCoCo

--- src/main/java/com/formation/Calculator.java
+++ src/main/java/com/formation/Calculator.java
@@ -8,21 +8,21 @@ package com.formation;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b + 6;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
-        return a - b + 6;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        return a * b + 6;
+        return a * b;
     }
 
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division par zéro impossible");
         }
-        return a + 6 / b;
+        return a / b;
     }
 }
